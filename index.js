c26f13bb1af3f5bd331fc98ce42bbd29f8e424a6
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-03-30 21:48:13 -0300

    [feat]: adicionou rotas /products e /sales no index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
+const express = require('express');
+const productsRoutes = require('./routes/productsRoutes');
+const salesRoutes = require('./routes/salesRoutes');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(express.json());
+app.use('/products', productsRoutes);
+app.use('/sales', salesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
