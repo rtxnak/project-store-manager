8c2462da98dc1a40791f5422de1b34ba457ecfcf
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-01 15:39:56 -0300

    [feat]: adicionou testes para os ENDPOINTS GET e POST nos caminhos /products e /sales

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -1,7 +1,107 @@
-describe('products models', () => {
-  describe('', () => {
-    it('', () => {
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/productsModel');
 
+const arrayOfAllSearchedProducts = [
+  {
+    id: 1,
+    name: "produto A",
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: "produto B",
+    quantity: 20
+  }
+];
+
+const oneProduct = [{
+  id: 1,
+  name: "produto A",
+  quantity: 10
+}];
+
+const newProduct = {
+  saleid: 1,
+  name: "produto A",
+  quantity: 10
+};
+
+describe('products models', () => {
+  describe('endpoint GET on /products', () => {
+    describe('products are found', () => {
+      before(async () => {
+        const execute = [arrayOfAllSearchedProducts];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return an array with products', async () => {
+        const response = await productsModel.getAll();
+
+        expect(response).to.be.equal(arrayOfAllSearchedProducts);
+      });
+    });
+    describe('no product is found', () => {
+      before(async () => {
+        const execute = [];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return undefined', async () => {
+        const response = await productsModel.getAll();
+        expect(response).to.be.equal(undefined);
+      });
+    });
+  });
+
+  describe('endpoint GET on /products/:id', () => {
+    describe('products ID is found', () => {
+      before(async () => {
+        const execute = [oneProduct];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return the product expected', async () => {
+        const response = await productsModel.getById(1);
+
+        expect(response).to.be.equal(oneProduct[0]);
+      });
+    });
+
+    describe('products ID is NOT found', () => {
+      before(async () => {
+        const execute = [[]];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return undefined', async () => {
+        const response = await productsModel.getById(1);
+
+        expect(response).to.be.equal(undefined);
+      });
+    });
+  });
+
+  describe('endpoint POST on /products', () => {
+    describe('product was created', () => {
+      before(async () => {
+        const execute = [{ insertId: 1 }];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return the product created', async () => {
+        const response = await productsModel.create(newProduct);
+
+        expect(response.id).to.be.equal(oneProduct[0].id);
+        expect(response.name).to.be.equal(oneProduct[0].name);
+        expect(response.quantity).to.be.equal(oneProduct[0].quantity);
+      });
     });
   });
 });
--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -1,7 +1,109 @@
-describe('sales models', () => {
-  describe('', () => {
-    it('', () => {
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const salesModel = require('../../../models/salesModel');
 
+const arrayOfAllSeachedSales = [
+  {
+    saleId: 1,
+    date: "2021-09-09T04:54:29.000Z",
+    productId: 1,
+    quantity: 2
+  },
+  {
+    saleId: 1,
+    date: "2021-09-09T04:54:54.000Z",
+    productId: 2,
+    quantity: 2
+  }
+];
+
+const newSale = {
+  ID: 1,
+  productId: 1,
+  quantity: 2
+};
+
+const newSaleReturn = {
+  saleId: 1,
+  productId: 1,
+  quantity: 2
+};
+
+
+describe('sales models', () => {
+  describe('endpoint GET on /sales', () => {
+    describe('sales are found', () => {
+      before(async () => {
+        const execute = [arrayOfAllSeachedSales];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return an array with sales', async () => {
+        const response = await salesModel.getAll();
+
+        expect(response).to.be.equal(arrayOfAllSeachedSales);
+      });
+    });
+
+    describe('no sales is found', () => {
+      before(async () => {
+        const execute = [];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return undefined', async () => {
+        const response = await salesModel.getAll();
+        expect(response).to.be.equal(undefined);
+      });
+    });
+  });
+
+
+  describe('endpoint GET on /sales/:id', () => {
+    describe('sale ID is found', () => {
+      before(async () => {
+        const execute = [arrayOfAllSeachedSales];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return the sale expected', async () => {
+        const response = await salesModel.getById(1);
+        expect(response).to.be.equal(arrayOfAllSeachedSales);
+      });
+    });
+
+    describe('sale ID is NOT found', () => {
+      before(async () => {
+        const execute = [];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return undefined', async () => {
+        const response = await salesModel.getById(1);
+        expect(response).to.be.equal(undefined);
+      });
+    });
+  });
+
+  describe('endpoint POST on /sales', () => {
+    describe('sales was created', () => {
+      before(async () => {
+        const execute = [{ insertId: 1 }];
+        sinon.stub(connection, 'execute').resolves(execute);
+      });
+      after(() => connection.execute.restore());
+
+      it('return the product created', async () => {
+        const response = await salesModel.create(newSale);
+        expect(response.saleId).to.be.equal(newSaleReturn.saleId);
+        expect(response.productId).to.be.equal(newSaleReturn.productId);
+        expect(response.quantity).to.be.equal(newSaleReturn.quantity);
+      });
     });
   });
 });
