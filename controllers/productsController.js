6f27498211eee0fc501b80906c08a6e526653082
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-01 04:05:20 -0300

    [fix]: corrigiu sintaxe de envio de mensagem nos erros 404

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -17,7 +17,7 @@ const getById = async (req, res) => {
     return res.status(200).json(products);
   } catch (err) {
     console.log(err);
-    return res.status(404).send({ message: 'Product not found' });
+    return res.status(404).json({ message: 'Product not found' });
   }
 };
 
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -17,7 +17,7 @@ const getById = async (req, res) => {
     return res.status(200).json(sales);
   } catch (err) {
     console.log(err);
-    return res.status(404).send({ message: 'Sale not found' });
+    return res.status(404).json({ message: 'Sale not found' });
   }
 };
 
